7b491787ca2aa39c4b3762050d3d558e5d0e904b
Salvatore Napoli <snapoli1998@gmail.com>
2021-01-27 21:08:57 +0100

    Miglioramento parziale

--- main.js
+++ main.js
@@ -3,49 +3,46 @@ Vue.config.devtools = true;
 new Vue ({
 	el: '.slider-wrapper',
 	data: {
-		currentImg: 'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
-
 		sliderImgs: [
 			'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
 			'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
 			'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
 			'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
 		],
+
+		currentImg: 0
 	},
 
 
 	methods: {
 		nextImg: function () {
-			let currentIdx = this.sliderImgs.indexOf(this.currentImg);
-			const navbar = document.getElementsByClassName('nav')[0];
-			const dotElements = navbar.children;
-
-
-			if (currentIdx === this.sliderImgs.length - 1) {
-				this.currentImg = this.sliderImgs[0];
-				dotElements[dotElements.length - 1].style.color = 'white';
-				dotElements[0].style.color = 'blue';
+			if (this.currentImg === this.sliderImgs.length - 1) {
+				this.currentImg = 0;
 			} else {
-				this.currentImg = this.sliderImgs[currentIdx + 1];
-				dotElements[currentIdx].style.color = 'white';
-				dotElements[currentIdx + 1].style.color = 'blue';
+				this.currentImg += 1;
 			}
 		},
 
 		prevImg: function () {
-			let currentIdx = this.sliderImgs.indexOf(this.currentImg);
-			const navbar = document.getElementsByClassName('nav')[0];
-			const dotElements = navbar.children;
-
-			if (currentIdx === 0) {
-				this.currentImg = this.sliderImgs[this.sliderImgs.length - 1];
-				dotElements[0].style.color = 'white';
-				dotElements[dotElements.length - 1].style.color = 'blue';
+			if (this.currentImg === 0) {
+				this.currentImg = this.sliderImgs.length - 1;
 			} else {
-				this.currentImg = this.sliderImgs[currentIdx - 1];
-				dotElements[currentIdx].style.color = 'white';
-				dotElements[currentIdx - 1].style.color = 'blue';
+				this.currentImg -= 1;
+			}
+		},
+
+		colorDots: function () {
+			var i = 0;
+			while (i < this.sliderImgs.length) {
+				console.log(i)
+				if (i === this.currentImg) {
+					return 'blue';
+				} else {
+					return 'white';
+				}
+				i++;
 			}
 		}
+
 	}
 });
